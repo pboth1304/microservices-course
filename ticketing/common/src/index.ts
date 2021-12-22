e1824be4d0af6f3e1c38c92bd7ae1f18e303ccc1
Pascal Bothner <pascalbothner@MacBook-Air-von-Pascal.local>
2021-12-22 21:46:34 +0100

    Updates

--- ticketing/common/src/index.ts
+++ ticketing/common/src/index.ts
@@ -1,5 +1,11 @@
-interface Color {
-  red: number;
-}
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error";
+export * from "./errors/database-connection-error";
+export * from "./errors/not-authorized-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
 
-export const test: Color = { red: 1 };
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handler";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validate-request";
